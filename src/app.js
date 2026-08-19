e4a3402a8325010e47cd5105bd7c9efb874b7086
Shaikh Ayesha <ayeshayy444@gmail.com>
2026-08-19 11:34:47 +0500

    feat: add health check route for local and deployed environments

--- src/app.js
+++ src/app.js
@@ -8,6 +8,15 @@ app.use(express.json({ limit: '50mb' }));
 // app.use(app.static('public'));
 app.use(cookieParser());
 
+// Health check route for local and deployed environments
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    statusCode: 200,
+    success: true,
+    message: "Welcome to Backend Practice"
+  });
+});
+
 // routes import
 import userRoutes from './routes/user.routes.js'
 
