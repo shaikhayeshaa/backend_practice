4869c1abf6625e3b97350dd64e5d41f2580a0fb2
Shaikh Ayesha <ayeshayy444@gmail.com>
2026-08-19 16:04:27 +0500

    bug fix

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -2,11 +2,11 @@ import { Router } from 'express';
 import {
     publishAVideo,
 } from "../controllers/video.controller.js"
-import {verifyJWT} from "../middlewares/auth.middleware.js"
+import {verifyJwt} from "../middlewares/auth.middleware.js"
 import {upload} from "../middlewares/multer.middleware.js"
 
 const router = Router();
-router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+router.use(verifyJwt); // Apply verifyJWT middleware to all routes in this file
 
 router
     .route("/")
