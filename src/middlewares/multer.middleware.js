5a1016d0da8dad7c477467aef281401c6d977649
Shaikh Ayesha <ayeshayy444@gmail.com>
2026-08-19 12:37:30 +0500

    refactor: improve multer middleware by enhancing upload directory handling and code consistency

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,17 +1,22 @@
-
 import multer from "multer";
+import fs from "fs";
+import path from "path";
+
+const uploadDirectory = path.resolve("public/temp");
+
+fs.mkdirSync(uploadDirectory, { recursive: true });
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/temp")
+  destination(req, file, cb) {
+    cb(null, uploadDirectory);
   },
-  filename: function (req, file, cb) {
-    //   cb(null, file.originalname)
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix)
-  }
-})
 
-export const upload = multer({
-  storage,
-})
+  filename(req, file, cb) {
+    const uniqueSuffix =
+      Date.now() + "-" + Math.round(Math.random() * 1e9);c
+
+    cb(null, `${file.fieldname}-${uniqueSuffix}`);
+  }
+});
+
+export const upload = multer({ storage });
